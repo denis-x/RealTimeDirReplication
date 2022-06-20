d0fa5ab898f52a472d6e5d288c3acd993bc65913
denis-x <56864423+denis-x@users.noreply.github.com>
2022-06-20 09:09:24 +0200

    Update __init__.py
    version update to 0.1.1

--- dirrepl/__init__.py
+++ dirrepl/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Denis Shuvalov'
 __description__ = 'Command line utility to replicate a content of origin directory in mirror directory in real time'
